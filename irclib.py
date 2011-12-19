3b63c88fd295d56ea496130de3f3dcfa7011490a
Andrei Z <b1naryth1ef@gmail.com>
2011-12-19 17:53:56 -0600

    Added hooks, bans/kicks, nick changes and auto-rejoins!
    Signed-off-by: Andrei Z <b1naryth1ef@gmail.com>

--- irclib.py
+++ irclib.py
@@ -4,12 +4,38 @@ import os, sys, time
 #LAMBDAZ!
 fromHost = lambda host: host.split('!')[0][1:]
 modez = {'+':True, '-':False}
+hookz = {}
+
+class Data():
+	def __init__(self, data):
+		self.data = data
+		self.__dict__.update(data)
+
+def hookFire(hook, data):
+	d = Data(data)
+	if hook in hookz.keys():
+		for i in hookz[hook]:
+			i(d)
 
 class User():
 	def __init__(self, name, voice=False, op=False):
 		self.name = name
+		self.aliasis = []
 		self.voice = voice
 		self.op = op
+
+	def changedName(self, newname):
+		self.aliasis.append(self.name)
+		self.name = newname
+
+class Penalty():
+	def __init__(self, user, reason, byuser, Type):
+		self.user = user #user OR hostmask
+		self.reason = reason
+		self.byuser = byuser
+		self.type = Type
+		self.time = time.time()
+		self.enabled = True
 
 class Channel():
 	def __init__(self, name):
@@ -17,6 +43,24 @@ class Channel():
 		self.users = {}
 		self.topic = ''
 		self.modes = {}
+		self.penalties = {}
+
+	def hasUser(self, user):
+		if user in self.users.keys(): return True
+		else: return False
+
+	def userNickChanged(self, user, newnick):
+		old = self.users[user]
+		old.changedName(newnick)
+		self.users[newnick] = old
+		del self.users[user]
+
+	def addPenalty(self, user, penalty):
+		if user in self.penalties.keys(): self.penalties[user].append(penalty)
+		else: self.penalties[user] = [penalty]
+
+	def userKicked(self, user, byuser, reason):
+		self.addPenalty(user, Penalty(user, None, reason, byuser, 'kick'))
 
 	def setTopic(self, topic, user):
 		self.topic = topic
@@ -25,9 +69,13 @@ class Channel():
 		self.modes[mode[1:]] = modez[mode[:1]]
 
 	def setUserMode(self, mode, user, byuser): 
-		if mode[1:] == 'v':
+		if mode[1:] == 'b' and mode[1:] == '+':
+			self.addPenalty(user, Penalty(user, reason, byuser, 'ban'))
+		elif mode[1:] == 'b' and mode[1:] == '+':
+			self.penalties[user].enabled = False
+		elif mode[1:] == 'v':
 			self.users[user].voice = modez[mode[:1]]
-		if mode[1:] == 'o':
+		elif mode[1:] == 'o':
 			self.users[user].voice = modez[mode[:1]]
 
 	def updateUsers(self, dic):
@@ -100,9 +148,11 @@ class Connection():
 		self.c.send('%s\r\n' % content)
 
 class Client():
-	def __init__(self, connection):
+	def __init__(self, connection, rejoin=True):
 		self.c = connection
 		self.channels = {}
+		self.nick = self.c.nick
+		self.rejoin = rejoin
 	
 	def quit(self, msg='B1naryth1ef Rocks!', full=True):
 		self.c.write('QUIT :%s' % msg)
@@ -114,6 +164,7 @@ class Client():
 	
 	def partChannel(self, channel, msg='G\'bye!'):
 		self.c.write('PART %s :%s' % (channel, msg))
+		del self.channels[channel]
 
 	def parse(self, inp):
 		def names(msg):
@@ -121,11 +172,13 @@ class Client():
 			chan = msg[1].split(' ')[4]
 			users = msg[2].split(' ')
 			self.channels[chan].updateUsers(updateList(users))
+			hookFire('names', {'nicks':users, 'chan':chan})
 
 		def topic(msg):
 			msg = msg.split(':', 2)
 			chan = msg[1].split(' ')[3]
 			topic = msg[2]
+			hookFire('topic', {'chan':chan, 'topic':topic})
 		
 		def part(msg):
 			msg = msg.split(' ')
@@ -134,6 +187,7 @@ class Client():
 			pmsg = msg[3][1:]
 			nick = fromHost(hostmask)
 			self.channels[chan].userPart(nick, pmsg, hostmask)
+			hookFire('part', {'hostmask':hostmask, 'nick':nick, 'msg':pmsg, 'chan':chan})
 
 		def join(msg):
 			msg = msg.split(' ')
@@ -141,6 +195,7 @@ class Client():
 			chan = msg[2]
 			nick = fromHost(hostmask)
 			self.channels[chan].userJoin(nick, hostmask)
+			hookFire('join', {'hostmask':hostmask, 'chan':chan, 'nick':nick})
 		
 		def mode(msg):
 			msg = msg.split(' ')
@@ -149,37 +204,71 @@ class Client():
 			fromuser = fromHost(hostmask)
 			if len(msg) == 4: #Channel Mode
 				mode = msg[3]
-				self.channels[chan].setMode(mode, fromuser)
-			elif len(msg) == 5: #User Mode
+				if fromuser != mode:
+					self.channels[chan].setMode(mode, fromuser)
+					hookFire('channelmode', {'hostmask':hostmask, 'fromnick':fromuser, 'mode':mode})
+			elif len(msg) >= 5: #User Mode
 				touser = msg[4]
 				mode = msg[3]
 				self.channels[chan].setUserMode(mode, touser, fromuser)
-				print 'FIRED'
+				hookFire('usermode', {'fromnick':fromuser, 'tonick':touser, 'mode':mode, 'chan':chan})
 
 		def setTopic(msg):
 			msg = msg.split(' ', 3)
 			hostmask = msg[0]
 			chan = msg[2]
 			newtopic = msg[3][1:]
-			user = fromHost(hostmask)
-			self.channels[chan].setTopic(newtopic, user)
+			nick = fromHost(hostmask)
+			self.channels[chan].setTopic(newtopic, nick)
+			hookFire('settopic', {'hostmask':hostmask, 'nick':nick, 'newtopic':newtopic})
 		
 		def msg(msg):
 			msg = msg.split(' ', 3)
 			hostmask = msg[0]
 			chan = msg[2]
 			msg = msg[3][1:]
-			user = fromHost(hostmask)
-			print '[%s] %s: %s' % (chan, user, msg)
+			nick = fromHost(hostmask)
+			print '[%s] %s: %s' % (chan, nick, msg)
+			hookFire('chansay', {'hostmask':hostmask, 'nick':nick, 'msg':msg, 'chan':chan})
+		
+		def kick(msg):
+			msg = msg.split(' ')
+			hostmask = msg[0]
+			chan = msg[2]
+			fromnick = fromHost(hostmask)
+			nick = msg[3]
+			reason = msg[4][1:]
+			if reason == nick:
+				reason = 'none given'
+			if self.nick == nick:
+				hookFire('uskicked', {'chan':chan, 'fromnick':fromnick})
+				del self.channels[chan]
+				if self.rejoin is True:
+					self.joinChannel(chan)
+			else:
+				self.channels[chan].userKicked(nick, fromnick, reason)
+				hookFire('kick', {'fromnick':fromnick, 'nick':nick, 'chan':chan, 'reason':reason})
+
+		def nick(msg):
+			msg = msg.split(' ')
+			hostmask = msg[0]
+			fromnick = fromHost(hostmask)
+			tonick = msg[2][1:]
+			for chan in self.channels.values():
+				if chan.hasUser(fromnick):
+					chan.userNickChanged(fromnick, tonick)
+			hookFire('nick_change', {'hostmask':hostmask, 'fromnick':fromnick, 'tonick':tonick})
 
 		inp = inp.split('\r\n')
 		for l in inp:
-			#print '[X]',l
+			print '[X]',l
 			line_type = l.strip().split(' ')
 			line = l.strip()
 			if len(line_type) >= 2:
 				if line_type[1] == 'PRIVMSG':
 					msg(line)
+				elif line_type[0] == 'PING':
+					self.c.write('PONG')
 				elif line_type[1] == '353':
 					names(line)
 				elif line_type[1] == '332':
@@ -192,3 +281,7 @@ class Client():
 					mode(line)
 				elif line_type[1] == 'TOPIC':
 					setTopic(line)
+				elif line_type[1] == 'KICK': 
+					kick(line)
+				elif line_type[1] == 'NICK':
+					nick(line)
