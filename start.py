fe62f894b3b409c90a51190546bda19272ea4ce2
Andrei Z <b1naryth1ef@gmail.com>
2012-01-26 00:29:47 -0600

    Removed bot aspects
    JAPIL is now a library only! The bot will be moved onto a new repo, and linked!
    
    Signed-off-by: Andrei Z <b1naryth1ef@gmail.com>

--- start.py
+++
@@ -1,9 +0,0 @@
-import example, urllib2
-
-f = urllib2.urlopen('http://bella.wbbmx.org/japil/updates')
-if float(f.read(10)) > example.version:
-	print 'There is an newer version!'
-	print 'Download it at https://github.com/b1naryth1ef/JAPIL/zipball/release'
-else:
-	print 'No newer versions found!'
-example.init()
